4b867ee3e342db1dbbc92eb7f153f5106d5714a1
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2024-08-29 16:07:33 +0530

    updates

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-app.get('/',(req, res)=>{
-    res.render('main');
+app.get('/signup',(req, res)=>{
+    res.render('Signup');
 });
+
+app.get('/login',(req,res)=>{
+    res.render('Login');
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./views/**/*.ejs", "./public/javaScripts/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#044B7F',
+        secondary:'#D3D3D3',
+        inputSection:'#E4E4E4'
+      },
+      boxShadow: {
+        custom: '0px 0px 5.2px rgba(0, 0, 0, 0.3)',
+      },
+      fontFamily: {
+        Hedon_Regular: ['Hedon_Regular', 'sans-serif'], // Example with a self-hosted font
+      },
+    },
   },
   plugins: [],
 }
